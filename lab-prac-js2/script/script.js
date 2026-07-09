86144ed70070ede8f4c3f81c6e48555aa57510b6
Mohammad Shihab Hossain <hossainmohammad@proton.me>
2026-07-09 08:31:11 +0000

    Add registration form with validation and styling

--- lab-prac-js2/script/script.js
+++ lab-prac-js2/script/script.js
@@ -3,18 +3,32 @@ function validateForm() {
     
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
+    let valid = true;
     
 
     if (username.includes('_') || username.includes('-')) {
         document.getElementById("usernameError").innerHTML = "Username cannot contain hyphens or underscores.";
-    } if (username.length < 5 || username.length > 15) {
+        valid = false;
+
+    }else if (username.length < 5 || username.length > 15) {
         document.getElementById("usernameError").innerHTML = "Username must be between 5 and 15 characters.";
+        valid = false;
+    }
+    else{
+        document.getElementById("usernameError").innerHTML = "";
     }
     if (password.length < 8) {
         document.getElementById("passwordError").innerHTML = "Password must be at least 8 characters long.";
+        valid = false;
     }
-    totalRegistrations++;
-    document.getElementById("total").innerHTML = "Total Registratons: " + totalRegistrations;
+    else{
+        document.getElementById("passwordError").innerHTML = "";
+    }
+
+    if (valid) {
+        totalRegistrations++;
+        document.getElementById("total").innerHTML = "Total Registrations: " + totalRegistrations;
+    }
     
     return false;
 }
